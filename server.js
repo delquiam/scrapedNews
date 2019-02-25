5a066b1212f578cd521806ff6acabd5a30f6253b
wonder woman <rdelquiam@gmail.com>
2019-02-25 15:07:04 -0800

    went back to og code

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get("/saved", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({ saved: true })
         .then(function (dbArticle) {
+            console.log(dbArticle);
             // If we were able to successfully find Articles, send them back to the client
             res.render("saved", { articles: dbArticle });
         })
@@ -105,21 +106,21 @@ app.get("/articles", function (req, res) {
 })
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
 // Route for saving/updating an Article
 app.put("/articles/saved/:id", function (req, res) {
     db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true })
@@ -152,7 +153,7 @@ app.post("/notes/saved/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
       .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({ _id: id }, { $push:{ note: dbNote._id} }, { new: true, upsert: true});
+        return db.Article.findOneAndUpdate({ _id: id }, { $push:{ note: dbNote._id} }, { new: true});
       })
       .then(function(dbArticle) {
         // If we were able to successfully update an Article, send it back to the client
@@ -163,9 +164,8 @@ app.post("/notes/saved/:id", function(req, res) {
         res.json(err);
       });
   });
-  
- 
-  
+
+
 
 // Delete a note
 app.delete("/notes/delete/:note_id/:article_id", function (req, res) {
